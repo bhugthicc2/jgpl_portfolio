bbaa7b34d88c9e6b2457e8a1c803b82536982bcc
Jesie Gapol <gapoljesie23@gmail.com>
2026-05-29 23:08:22 +0800

    Added new image asset to projects.js

--- data/projects.js
+++ data/projects.js
@@ -191,6 +191,7 @@ export const projects = [
       'assets/images/projects/cvms/mobile/vehicle_info_scrolled.png',
       'assets/images/projects/cvms/mobile/report_modal.png',
       'assets/images/projects/cvms/mobile/choose_violation.png',
+      'assets/images/projects/cvms/mobile/activity.png',
       'assets/images/projects/cvms/mobile/drawer.png',
 
     ],
